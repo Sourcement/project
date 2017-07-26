048bcfb3e2e298cb4cfef0f978f9c2a25fe55b43
Mark Selyaev <mark.selyaeff@yandex.ru>
2017-07-26 18:00:56 +0300

    Added redirect to landing

--- gsl_project/urls.py
+++ gsl_project/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from .views import index
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^landing/', include('landing.urls')),
+    url(r'^$', index),
 ]
